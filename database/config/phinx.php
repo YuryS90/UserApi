2bcd04267f41c3dda5914595598817de2b751b5d
Yury <59015519+YuryS90@users.noreply.github.com>
2023-03-18 15:51:14 +0300

    v0.1 Create Db
    add phinx + dotenv /
    add table users /
    add teble roles /
    add fk

---
+++ database/config/phinx.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+// Сохранение параметров в $_ENV
+(\Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2)))->load();
+
+return
+    [
+        'paths' => [
+            'migrations' => __DIR__ . '/../migrations',
+        ],
+
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_environment' => 'dev',
+
+            'dev' => [
+                'adapter' => $_ENV['DB_CONNECTION'],
+                'host' => $_ENV['DB_HOST'],
+                'name' => $_ENV['DB_DATABASE'],
+                'user' => $_ENV['DB_USERNAME'],
+                'pass' => $_ENV['DB_PASSWORD'],
+                'port' => $_ENV['DB_PORT'],
+                'charset' => 'utf8',
+            ],
+
+        ],
+
+        'version_order' => 'creation'
+    ];
