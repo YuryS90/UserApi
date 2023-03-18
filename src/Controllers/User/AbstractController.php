b22b48085034564cc1cbfebad9b75906d9e4af81
Yury <59015519+YuryS90@users.noreply.github.com>
2023-03-18 14:28:15 +0300

    v0.0.1 refactoring

---
+++ src/Controllers/User/AbstractController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+abstract class AbstractController
+{
+    protected Request $request;
+    protected Response $response;
+    protected array $args;
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->args = $args;
+
+        return $this->run();
+    }
+
+    abstract protected function run(): Response;
+}
--- src/Controllers/User/AddController.php
+++ src/Controllers/User/AddController.php
@@ -3,20 +3,11 @@
 namespace App\Controllers\User;
 
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
-class AddController
+class AddController extends AbstractController
 {
-    private Request $request;
-    private Response $response;
-    private $args;
-
-    public function __invoke(Request $request, Response $response, array $args)
+    public function run(): Response
     {
-        $this->request = $request;
-        $this->response = $response;
-        $this->args = $args;
-
         $this->response->getBody()->write('test');
 
         return $this->response;
