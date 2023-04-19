4d8022b7633100c120d5fb29c7afc7ee45b5167f
Yury <59015519+YuryS90@users.noreply.github.com>
2023-04-19 18:09:26 +0300

    v0.2.1 Help for working with DI / adding helpers (debug)

---
+++ src/Common/ContainerTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Common;
+
+use Psr\Container\ContainerInterface;
+
+trait ContainerTrait
+{
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __get($name)
+    {
+        if (!$this->container->has($name)) {
+            return null;
+        }
+
+        return $this->container->get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        $this->container->set($name, $value);
+    }
+}
