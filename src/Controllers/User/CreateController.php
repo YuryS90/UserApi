2ab61b51b59644ae6e60878626cd7a35dcf90695
Yury <59015519+YuryS90@users.noreply.github.com>
2023-07-02 12:15:11 +0300

    v0.3.3.4 refactoring
    +Вынесен в отдельный модуль отправка почты

--- src/Controllers/User/CreateController.php
+++ src/Controllers/User/CreateController.php
@@ -3,7 +3,6 @@
 namespace App\Controllers\User;
 
 use App\Controllers\AbstractController;
-use PHPMailer\PHPMailer\PHPMailer;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class CreateController extends AbstractController
@@ -13,87 +12,47 @@ class CreateController extends AbstractController
      */
     public function run(): Response
     {
-        // План предварительной регистрации
-        // 1 Поймал данные
-        // 2 Обработал их, если ошибка, вернул её (статус 400)
-        // 3 Сгенерироривал пароль
-        // 4 1-й этап добавления со сгенерированным паролем
-        // 5 Отправка на почту пароля
-        // 6 Вернул статус 201
-
         // 1
         // request = $this->request->getParsedBody();
-
-        // 1 (временное)
         $request = [
-            'login' => "Sp2wN@",
+            'login' => "Sp2wN",
             'email' => "yurkesson@yandex.by",
         ];
 
         // 2
         $error = $this->validClass->validated($request, $this->validate['rules']['signUp']);
 
-        if ($error['error']) {
-            return $this->responseJson($error, 400);
+        if (!$error['error']) {
+
+            // 3 Генерация пароля
+            //$passwordSend = $this->genClass->password(12);
+            $passwordSend = "123";
+
+            //$data = require 'app/payload.php';
+            //$this->dd($data['createUser']);
+
+            // 4
+            $payload = [
+                'login' => $request['login'],
+                'email' => $request['email'],
+                'pwd' => password_hash($passwordSend, PASSWORD_DEFAULT),
+                'roles_id' => 3
+            ];
+
+            //$this->userRepo->insertOrUpdate($payload);
+
+            // 5 Отправка письма с паролем
+            $send = $this->mailClass->sendEmail($request['email'], [
+                'login' => $request['login'],
+                'pwdSend' => $passwordSend
+            ]);
+
+            $this->dd($send);
+
+            return $this->responseJson([
+                'message' => "Регистрация прошла успешно. Пароль был отправлен на {$request['email']}"
+            ], 201);
         }
-
-        // 3 Генерация и хэширование пароля
-        $password = password_hash($this->genClass->password(12), PASSWORD_DEFAULT);
-        $this->dd($password);
-
-        // 5 Отправка письма с паролем
-        $this->sendEmail($request['email'], $password);
-
-        // Отображение сообщения об успешной предварительной регистрации
-        // Отправка ответа
-        // должен получить это сообщение по слагу (новая таблица? id slug settinggs формат json(варчар)(key(на каком языке) значение перевод сооббщения))
-        // т.е. новое обращение к БД
-        $responseData = [
-            'message' => "Регистрация прошла успешно. Пароль был отправлен на {$request['email']}"
-        ];
-
-        return $this->responseJson($responseData, 201);
-    }
-
-
-    private function sendEmail($email, $password)
-    {
-        $mail = new PHPMailer(true);
-
-        try {
-            // Настройки SMTP
-            // Метод указывает, что используется SMTP-сервер для отправки писем
-            $mail->isSMTP();
-
-            // Адрес и порт SMTP-сервера
-            $mail->Host = 'smtp.mail.ru';
-            $mail->Port = 465;
-
-            // Протокол шифрования SSL/TLS (тип безопасного соединения)
-            $mail->SMTPSecure = 'ssl';
-
-            // Параметры аутентификации, которые используются для авторизации на SMTP-сервере.
-            // true - определяет, требуется ли аутентификация для подключения
-            $mail->SMTPAuth = true;
-            $mail->Username = 'projectyury@mail.ru';
-            $mail->Password = 'TA8dP7YQgm2MXRFSxncP';
-
-            // Чтобы почта приходила без иероглифов
-            $mail->CharSet = "UTF-8";
-
-            // Отправитель и получатель
-            $mail->setFrom('projectyury@mail.ru', 'User API');
-            $mail->addAddress($email);
-
-            // Тема и содержимое письма
-            $mail->Subject = 'Ваш пароль для MyApp';
-            $mail->Body = 'Ваш новый пароль: ' . $password;
-
-            // Отправка письма
-            $mail->send();
-
-        } catch (\Exception $e) {
-            die(json_encode("При отправке письма произошла ошибка: {$mail->ErrorInfo}", JSON_UNESCAPED_UNICODE));
-        }
+        return $this->responseJson($error, 400);
     }
 }
